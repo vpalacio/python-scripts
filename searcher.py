7e5eb9b3270e60f16662189a1a63819e738b98bf
Victor Palacio <vpalacio@ise.com>
2016-01-24 21:49:34 -0500

    adding __main__ and a helpful message is no arguments are passed in

--- searcher.py
+++ searcher.py
@@ -1,28 +1,41 @@
 #!/usr/bin/env python
 
+
 """Searcher.py: A python script to filter between 2 points in a file."""
 
+
 import argparse
+import sys
+
 
 __author__ = "Victor Palacio"
 __license__ = "GPL"
 __version__ = "1.0"
 __email__ = "Github - @vpalacio"
 
-parser = argparse.ArgumentParser(
-   description='Python script to filter between 2 points in a file')
-parser.add_argument('-file', help='file to search through')
-parser.add_argument('-start', help='starting point')
-parser.add_argument('-end', help='end point')
 
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(
+        description='Python script to filter between 2 points in a file')
+    parser.add_argument('-file', help='file to search through')
+    parser.add_argument('-start', help='starting point')
+    parser.add_argument('-end', help='end point')
 
-with open(args.file) as infile:
-    copy = False
-    for line in infile:
-        if line.strip() == args.start:
-            copy = True
-        elif line.strip() == args.end:
-            copy = False
-        elif copy:
-            print line,
+    if len(sys.argv) < 1:
+        parser.print_help()
+        sys.exit(1)
+
+    args=parser.parse_args()
+
+    with open(args.file) as infile:
+        copy = False
+        for line in infile:
+            if line.strip() == args.start:
+                copy = True
+            elif line.strip() == args.end:
+                copy = False
+            elif copy:
+                print line,
+
+if __name__ == '__main__':
+    main()
